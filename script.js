7f4d1e8b4b6bdeeb5440bc467b7e63ea932429c4
nabihakhan6t4 <khannabiha923@gmail.com>
2024-09-22 17:29:02 +0300

    completed

---
+++ script.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".auto-type", {
+  strings: [
+    "Fully Customizable ",
+    "High Converting",
+    "Next Generation",
+    "Fastest",
+    "Responsive",
+    "Clean & Modern",
+  ],
+  typeSpeed: 50, // Speed of typing (ms)
+  backSpeed: 25, // Speed of backspacing (ms)
+  loop: true, // Loops the typing effect
+});
