15218ad52bb2a9176864e17c789ec07b16d7a812
nabihakhan6t4 <khannabiha923@gmail.com>
2024-09-24 17:52:55 +0300

    adding demo section

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ var typed = new Typed(".auto-type", {
   backSpeed: 25, // Speed of backspacing (ms)
   loop: true, // Loops the typing effect
 });
+
+let obj = {
+  picture: "images/fashion-3.jpg",
+};
+
+// Using Object.keys to iterate through the object's keys
+for (var key of Object.keys(obj)) {
+  var box = document.querySelector(".box");
+  box.innerHTML = `<div>
+  <img src = '${key}' alt=''/>
+  </div>`;
+  console.log(key + ": " + obj[key]); // logs the key and its value
+}
+
+// Alternatively, using for...in
+for (var key in obj) {
+  console.log(key + ": " + obj[key]); // logs the key and its value
+}
+
+console.log("hi");
