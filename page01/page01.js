dd680591d8e17490952815dc720763a2ce9c678d
nabihakhan6t4 <khannabiha923@gmail.com>
2024-10-02 17:55:19 +0300

    added more sections

--- page01/page01.js
+++ page01/page01.js
@@ -8,3 +8,21 @@ document.addEventListener("mousemove", function (e) {
   cursor2.style.left = e.clientX + "px";
   cursor2.style.top = e.clientY + "px";
 });
+
+
+// Select all categories
+const categories = document.querySelectorAll('.category');
+
+// Add click event listeners to each category
+categories.forEach(category => {
+  category.addEventListener('click', () => {
+    // Remove active class from all categories
+    categories.forEach(cat => cat.classList.remove('active-category'));
+    // Add active class to the clicked category
+    category.classList.add('active-category');
+  });
+});
+
+
+
+alert("hello world")
